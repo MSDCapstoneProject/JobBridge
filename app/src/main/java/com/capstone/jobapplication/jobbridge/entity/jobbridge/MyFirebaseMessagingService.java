9394a816cfc8c501c22c841ccca6c5f198d1c9b9
sun7016 <sarachoi7016@gmail.com>
2017-06-14 00:21:51 -0400

    job list view and job detail view

--- app/src/main/java/com/capstone/jobapplication/jobbridge/entity/jobbridge/MyFirebaseMessagingService.java
+++ app/src/main/java/com/capstone/jobapplication/jobbridge/entity/jobbridge/MyFirebaseMessagingService.java
@@ -48,7 +48,7 @@ public class MyFirebaseMessagingService  extends FirebaseMessagingService {
 
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob();
+                //scheduleJob();
             } else {
                 // Handle message within 10 seconds
                 handleNow();
