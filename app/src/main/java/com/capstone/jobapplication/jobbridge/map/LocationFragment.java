b1d88a063b56889ee8586d8520ef625e94fb9f40
sun7016 <sarachoi7016@gmail.com>
2017-06-24 15:54:43 -0400

    add map java file

--- app/src/main/java/com/capstone/jobapplication/jobbridge/map/LocationFragment.java
+++ app/src/main/java/com/capstone/jobapplication/jobbridge/map/LocationFragment.java
@@ -1,22 +1,41 @@
 package com.capstone.jobapplication.jobbridge.map;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 
 import com.capstone.jobapplication.jobbridge.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * Created by Sarah on 2017-06-23.
  */
 
-public class LocationFragment  extends AppCompatActivity {
+public class LocationFragment extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
