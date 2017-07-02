26392cd622e27418cdd0f8fbe27f40a32d478b29
sun7016 <sarachoi7016@gmail.com>
2017-07-01 23:20:27 -0400

    drawing route on map java file

--- app/src/main/java/com/capstone/jobapplication/jobbridge/map/LocationFragment.java
+++ app/src/main/java/com/capstone/jobapplication/jobbridge/map/LocationFragment.java
@@ -1,6 +1,7 @@
 package com.capstone.jobapplication.jobbridge.map;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -19,6 +20,9 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,6 +31,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -96,6 +102,8 @@ public class LocationFragment extends FragmentActivity implements OnMapReadyCall
         LatLng where2 = new LatLng(location2.latitude, location2.longitude);
         getRoute(where);
         getRoute(where2);
+
+        
     }
 
     public void getRoute(LatLng point) {
@@ -168,6 +176,7 @@ public class LocationFragment extends FragmentActivity implements OnMapReadyCall
 
     /**
      * A method to download json data from url
+     * ownloadUrl is used to fecth url from web service and result is parsed using ParserTask (Async Task).
      */
     private String downloadUrl(String strUrl) throws IOException {
         String data = "";
@@ -208,6 +217,11 @@ public class LocationFragment extends FragmentActivity implements OnMapReadyCall
     }
 
     // Fetches data from url passed
+    /*
+    * AsyncTask is an abstract class provided by Android which helps us to use the UI thread properly.
+    * This class allows us to perform long/background operations and show its result on the UI thread without having to manipulate threads.
+     * Here doInBackground task will be implemented in background and onPostExecute will be shown on GUI.
+    * */
     private class FetchUrl extends AsyncTask<String, Void, String> {
 
         @Override
@@ -224,6 +238,90 @@ public class LocationFragment extends FragmentActivity implements OnMapReadyCall
                 Log.d("Background Task", e.toString());
             }
             return data;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+//doInBackground will actually parse the data and onPostExecute will draw route on Google Maps.
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.RED);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
         }
     }
 }
