e3d16c913d6293db87e0e6eccee41bb746b70025
sun7016 <sarachoi7016@gmail.com>
2017-09-28 18:16:41 -0400

    google login

--- app/src/main/java/com/capstone/jobapplication/jobbridge/fragments/AboutFragment.java
+++ app/src/main/java/com/capstone/jobapplication/jobbridge/fragments/AboutFragment.java
@@ -10,9 +10,9 @@ import android.view.ViewGroup;
 
 import com.capstone.jobapplication.jobbridge.ProfileActivity;
 import com.capstone.jobapplication.jobbridge.R;
+import com.capstone.jobapplication.jobbridge.SignInActivity;
 import com.capstone.jobapplication.jobbridge.SubscriptionActivity;
 import com.capstone.jobapplication.jobbridge.fcm.FcmActivity;
-import com.capstone.jobapplication.jobbridge.map.LocationFragment;
 
 /**
  * Created by Aicun on 5/31/2017.
@@ -47,6 +47,8 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
             case R.id.signup:
                 //// TODO: 6/23/2017 change to sign up story
                 //sendTokenTOServer();
+                Intent signup = new Intent(getActivity(), SignInActivity.class);
+                startActivity(signup);
                 break;
             case R.id.setting:
                 Intent intentSetting = new Intent(getActivity(), FcmActivity.class);
